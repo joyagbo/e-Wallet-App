63a702b6a26c854ef09a4c43a981675e1c3cc96d
joy <joyagbo49@gmail.com>
2023-03-24 04:15:10 +0100

    Added auth folder to verify and send token to users.

--- backend/index.js
+++ backend/index.js
@@ -15,8 +15,10 @@ app.use(cors())
 //console.log(process.env.DATABASE_USER)
 
 console.log(bcrypt.hashSync("p@ssword", saltRounds))
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
+
 app.use('/', routeManger)
 
 app.listen(port, () => {
