643013da741c74bf805a649cd33d30380ead7407
joy <joyagbo49@gmail.com>
2023-03-29 18:27:09 +0100

    added dashboard route

--- backend/routes/rts.js
+++ backend/routes/rts.js
@@ -1,13 +1,11 @@
 const express = require('express')
-const { register, login } = require('../controllers/customerControls.js')
-
+const { register, login, dashboard } = require('../controllers/customerControls.js')
+const { verifyAuth } = require('../middleware/auth.js')
 
 const routeManger = express.Router()
 //routeManger.get('/',register)
-
 routeManger.post('/registerCustomer',register)
-
 routeManger.post('/Auth',login)
-
+routeManger.post('/dashboard',verifyAuth,dashboard)
   
 module.exports ={routeManger}
