60d7fe88694f68da99512203874c3e7dd3247c29
joyagbo <joyagbo49@gmail.com>
2025-07-14 19:35:46 +0100

    updated db credentials

--- backend/config/connections.js
+++ backend/config/connections.js
@@ -1,8 +1,20 @@
 const Sequelize = require('sequelize')
 const dotenv = require('dotenv')
 dotenv.config()
-const sequelize = new Sequelize(process.env.DATABASE,process.env.DATABASE_USER,"08134715847",{
-    dialect:"mysql", host:"localhost"
-});
+const sequelize = new Sequelize(
+    process.env.DATABASE,
+    process.env.DATABASE_USER,
+    process.env.DATABASE_PASSWORD,
+    {
+        dialect: "mysql",
+        host: process.env.DATABASE_HOST,
+    });
+
+sequelize.authenticate()
+    .then(() => console.log('Connected to MySQL Database'))
+    .catch((err) => console.error('Database connection failed:', err));
 
 module.exports = sequelize;
+
+
+
