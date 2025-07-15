3302a27de5ff3564fb87c7c3fedbbd1b7b0bf08b
joyagbo <joyagbo49@gmail.com>
2025-07-15 22:28:12 +0100

    formatted file

--- frontend/index.js
+++ frontend/index.js
@@ -3,53 +3,53 @@ const app = express()
 const port = 4000
 
 var transactions = [
-    {
-        text: "Lorem Ipsum is simply dummy text of the printing",
+  {
+    text: "Lorem Ipsum is simply dummy text of the printing",
     date: "24 June 2020",
-    due:"Tomorrow"
+    due: "Tomorrow"
 
-    },
+  },
 
-    {
-        text: "Lorem Ipsum is simply dummy text of the printing",
+  {
+    text: "Lorem Ipsum is simply dummy text of the printing",
     date: "24 June 2020",
-    due:"Tomorrow"
+    due: "Tomorrow"
 
-    },
+  },
 
-    {
-        text: "Lorem Ipsum is simply dummy text of the printing",
+  {
+    text: "Lorem Ipsum is simply dummy text of the printing",
     date: "24 June 2020",
-    due:"Tomorrow"
-    },
+    due: "Tomorrow"
+  },
 
-    {
-        text: "Lorem Ipsum is simply dummy text of the printing",
+  {
+    text: "Lorem Ipsum is simply dummy text of the printing",
     date: "24 June 2020",
-    due:"Tomorrow"
+    due: "Tomorrow"
 
-    }
+  }
 ]
 
-var users =[{username:"Joy",email:"joy@gmail.com",balance:500},{username:"",email:""}]
+var users = [{ username: "Joy", email: "joy@gmail.com", balance: 500 }, { username: "", email: "" }]
 
 app.use(express.static('public')) //code for making styles show
 
 app.set('view engine', 'ejs')
 
 app.get('/dashboard', (req, res) => { //routes
-  
-  res.render('index',{users:users,transactions:transactions})
+
+  res.render('index', { users: users, transactions: transactions })
 })
 
 app.get('/', (req, res) => {
-    res.render('login')
-   })
+  res.render('login')
+})
 
- app.get('/register', (req, res) => {
-    res.render('register')
-  })
-  
+app.get('/register', (req, res) => {
+  res.render('register')
+})
+
 app.listen(port, () => {
-  console.log('server is running')
+  console.log(`server is running on port ${port}`)
 })
