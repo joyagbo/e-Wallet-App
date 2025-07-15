618531e8964ae8e7f98717a852e0024cb9d36cae
joyagbo <joyagbo49@gmail.com>
2025-07-15 22:29:48 +0100

    rename file

--- backend/routes/customerRoute.js
+++ backend/routes/customerRoute.js
@@ -4,8 +4,8 @@ const { verifyAuth } = require('../middleware/auth.js')
 
 const routeManger = express.Router()
 //routeManger.get('/',register)
-routeManger.post('/registerCustomer',register)
-routeManger.post('/Auth',login)
-routeManger.post('/dashboard',verifyAuth,dashboard)
-  
-module.exports ={routeManger}
+routeManger.post('/registerCustomer', register)
+routeManger.post('/Auth', login)
+routeManger.post('/dashboard', verifyAuth, dashboard)
+
+module.exports = { routeManger }
